86e2ba14f156c8dceeb1abeb02af84865c867e2a
Ilay Morozov <morozoff1994@yandex.ru>
2022-06-19 15:43:48 +0300

    fix: refactor userService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,26 +3,25 @@ import { CustomError } from '../error/error';
 import { IUserModel, Messages } from "../model/users";
 import { STATUS_CODES } from '../utils/writeHeader';
 import { Validator } from '../validator/validator';
+import { Repository } from '../repository/repository';
 
 
 export class UserService {
-  private users: IUserModel[] = [];
+  private repository = new Repository();
   private validator = new Validator();
-
-  constructor() {
-    this.users = [];
-  }
 
   findAll(): Promise<IUserModel[]> {
     return new Promise((resolve) => {
 
-      resolve(this.users);
+      const users = this.repository.findAll();
+      resolve(users);
     })
   }
 
   findById(id: string): Promise<IUserModel> {
     return new Promise((resolve, reject) => {
-      const user = this.users.find((item) => item.id === id);
+      const user = this.repository.findById(id);
+      
       if (user) {
         resolve(user);
       } else {
@@ -36,20 +35,15 @@ export class UserService {
     return new Promise((resolve) => {
       const normalizedBody = this.normalizeBody(user);
       const newUser = { id: uuidv4(), ...normalizedBody };
-      this.users.push(newUser);
-      resolve(newUser);
+      const newUserFromRep = this.repository.create(newUser);
+      resolve(newUserFromRep);
     });
   }
 
   update(id: string, user: IUserModel): Promise<IUserModel> {
     return new Promise(async (resolve, reject) => {
       try {
-        const updatedUserIndex = this.users.findIndex((item) => item.id === id);
 
-        if (updatedUserIndex === -1) {
-          const err = new CustomError(Messages.UserNotFound, STATUS_CODES.NotFound);
-          reject(err);
-        }
         const currentDataUser = await this.findById(id);
   
         const { age, hobbies, username } = user;
@@ -59,9 +53,14 @@ export class UserService {
           hobbies: hobbies || currentDataUser.hobbies,
           username: username || currentDataUser.username
         }
-        this.users[updatedUserIndex] = { id, ...newUpdatedBody };
-  
-        resolve(this.users[updatedUserIndex]);
+        const updatedUser = this.repository.update(id,newUpdatedBody);
+
+        if (!updatedUser) {
+          const err = new CustomError(Messages.UserNotFound, STATUS_CODES.NotFound);
+          reject(err);
+        }
+
+        resolve(updatedUser);
       } catch (error) {
         const err = new CustomError(Messages.UserNotFound, STATUS_CODES.NotFound);
         reject(err);
@@ -71,8 +70,7 @@ export class UserService {
 
   delete(id: string): Promise<IUserModel> {
     return new Promise((resolve, reject) => {
-      const deletedUser = this.users.find((el) => el.id === id);
-      this.users = this.users.filter((item) => item.id !== id);
+      const deletedUser = this.repository.delete(id)
       if (deletedUser) {
         resolve(deletedUser);
       } else {
